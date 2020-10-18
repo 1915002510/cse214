4a82498c7a9f72715d5a313ba2c75143206ee021
Sanower Hossain Rabbi <71255389+1915002510@users.noreply.github.com>
2020-10-19 03:09:00 +0600

    Add files via upload

---
+++ Problem Set 1- Array/5.c
@@ -0,0 +1,15 @@
+/* 5. Print entire array each element separated by space.
+Name: Sanower Hossain Rabbi
+ID: 1915002510
+*/
+#include <stdio.h>
+int main ()
+{
+int x[100] = {36,19,3,15,5,6,18,8,1,20}, max_size = 100, size = 10, i;
+
+for (i=0; i<size; i++)
+    {
+        printf("%d ",x[i]);
+    }
+return 0;
+}
