db5dd45216ae9def5cde97547abd2a85f77b4398
Sanower Hossain Rabbi <71255389+1915002510@users.noreply.github.com>
2020-10-19 03:08:38 +0600

    Add files via upload

---
+++ Problem Set 1- Array/4.c
@@ -0,0 +1,14 @@
+/* 4. Print entire array each element in newline.
+Name: Sanower Hossain Rabbi
+ID: 1915002510
+*/
+#include <stdio.h>
+int main (){
+int x[100] = {36,19,3,15,5,6,18,8,1,20}, max_size = 100, size = 10, i;
+
+for (i= 0; i<size;i++)
+    {
+    printf("%d \n", x[i]);
+    }
+return 0;
+}
