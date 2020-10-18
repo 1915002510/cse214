3f8094913cf44bb4bcd055a4d33c182eba4db57e
Sanower Hossain Rabbi <71255389+1915002510@users.noreply.github.com>
2020-10-19 05:43:38 +0600

    Add files via upload

---
+++ Problem Set 2- Array/14.c
@@ -0,0 +1,56 @@
+/* 14.	Delete a value after X (take input X from user) from the array shift all other value to left. If X is not present in the given array print “Element Not Found”. Only delete the first occurrence of the value.
+Name: Sanower Hossain Rabbi
+ID: 1915002510
+*/
+#include<stdio.h>
+int main()
+{
+    int a[100] = {36,19,3,15,5,6,18,8,10,20}, max_size = 100, size = 10, i,X,y=-1;
+
+    printf("Enter the value of X :\n");
+    scanf("%d",&X);
+
+    for(i=0;i<size;i++)
+    {
+      if(X==a[i])
+        {
+         y=i;
+         y++;
+         break;
+        }
+    }
+
+    if(y!=-1)
+    {
+        if(y>=size)
+        {
+          printf("Array is above the limit.");
+        }
+        else{
+        printf("Before Operation :\n");
+        for(i=0;i<size;i++)
+        {
+           printf("%d ",a[i]);
+        }
+
+        printf("\n");
+
+        for(i=y;i<size-1;i++)
+            {
+              a[i]=a[i+1];
+            }
+            size--;
+        printf("After Operation :\n");
+        for(i=0;i<size;i++)
+        {
+           printf("%d ",a[i]);
+        }
+
+        }
+    }
+
+    else{
+        printf("Element Not Found");
+    }
+    return 0;
+}
