e0af7007a73927f6d64deea679f0e485aeafb773
Sanower Hossain Rabbi <71255389+1915002510@users.noreply.github.com>
2020-10-19 05:37:10 +0600

    Add files via upload

---
+++ Problem Set 2- Array/2.c
@@ -0,0 +1,33 @@
+/* 2. Insert a value X (take input X from user) in the array at the end of currently stored elements.
+Name: Sanower Hossain Rabbi
+ID: 1915002510
+*/
+#include<stdio.h>
+int main()
+{
+    int a[100] = {36,19,3,15,5,6,18,8,10,20}, max_size = 100, size = 10, i, X;
+
+    printf("Enter the value of X :\n");
+    scanf("%d",&X);
+
+    printf("Before Operation :\n");
+    for(i=0;i<size;i++)
+    {
+       printf("%d ",a[i]);
+    }
+
+    printf("\n");
+
+    a[size] = X;
+    size++;
+
+    printf("After Operation :\n");
+    for(i=0;i<size;i++)
+    {
+       printf("%d ",a[i]);
+    }
+    return 0;
+}
+
+
+
