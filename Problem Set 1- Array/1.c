770aca89d9aca183b257833e26eafa4ab06517b7
Sanower Hossain Rabbi <71255389+1915002510@users.noreply.github.com>
2020-10-19 03:06:58 +0600

    Add files via upload

---
+++ Problem Set 1- Array/1.c
@@ -0,0 +1,18 @@
+/* 1. Scan N (take input N from user) values from user into an array.
+Name: Sanower Hossain Rabbi
+ID: 1915002510
+*/
+#include <stdio.h>
+int main (){
+
+int x [100], max_size=100, size=0, N;
+
+printf("Enter the value of N: \n");
+scanf("%d",&N);
+
+if (N>max_size)
+{printf("Overflow");}
+
+return 0;
+
+}
